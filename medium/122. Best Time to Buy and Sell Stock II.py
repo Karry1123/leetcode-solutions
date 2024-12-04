fa09899909448a80b1fb1dc9f08447ab2f0ebb7b
Karry1123 <86029350+Karry1123@users.noreply.github.com>
2024-12-04 20:38:28 +0800

    Delete medium directory

--- medium/122. Best Time to Buy and Sell Stock II.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from typing import Optional
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        sum=0
-        for i in range(1,len(prices)):
-            if prices[i]-prices[i-1]>0:
-                sum+=prices[i]-prices[i-1]
-        return sum
