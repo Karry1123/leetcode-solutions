933ebfab0d8fa09b3dfb1fda41bf87636deb2920
Karry1123 <1953745794@qq.com>
2024-12-04 22:38:51 +0800

    Add new solution to medium

---
+++ medium/11. Container With Most Water.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l=len(height)
+        left=0
+        right=l-1
+        maxheight=max(height)
+        maxvolume=0
+        while left<right:
+            width=right-left
+            if maxheight*width < maxvolume:
+                break
+            l_h=height[left]
+            r_h=height[right]
+            real_height=min(l_h,r_h)
+            realvolume=real_height*width
+            if realvolume>maxvolume:
+                maxvolume=realvolume
+            if l_h < r_h:
+                left += 1
+            else:
+                right -= 1
+        return maxvolume
