9fb0dc2eb88a776ecb0ca763e677795f8b7eea7e
Karry1123 <1953745794@qq.com>
2024-12-25 01:04:44 +0800

    update 39

---
+++ medium/39. Combination Sum.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        ans = []
+        path = []
+
+        def dfs(i: int, left: int) -> None:
+            if left == 0:
+                # 找到一个合法组合
+                ans.append(path.copy())
+                return
+
+            if i == len(candidates) or left < candidates[i]:
+                return
+
+            # 不选
+            dfs(i + 1, left)
+
+            # 选
+            path.append(candidates[i])
+            dfs(i, left - candidates[i])
+            path.pop()  # 恢复现场
+
+        dfs(0, target)
+        return ans
