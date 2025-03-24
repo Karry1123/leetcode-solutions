0b55262191f76e5b40dd82c8c6660ed619e2254f
Karry1123 <1953745794@qq.com>
2025-03-25 00:59:36 +0800

    update 105

---
+++ medium/105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from typing import List
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        dic={}
+        for i in range(len(inorder)):
+            dic[inorder[i]]=i
+        def dfs(preleft,preright,inleft,inright):
+            if preleft>preright or inleft>inright:
+                return None
+            root = TreeNode(preorder[preleft])  # 直接创建一个节点，val=preorder[preleft]
+            pindex=dic.get(root.val)
+            root.left=dfs(preleft+1,pindex-inleft+preleft,inleft,pindex-1)
+            root.right=dfs(pindex-inleft+preleft+1,preright,pindex+1,inright)
+            return root
+        return dfs(0,len(preorder)-1,0,len(inorder)-1)
